f89e31a8fc18b6e40929fcfec7c061c323528c4e
Huy Nguyen <huyhuy8122001@gmail>
2024-02-02 18:19:42 +0700

    sip blind transfer

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       LOGLEVEL: 'info',
-      HTTP_PORT: 3000,
-      JAMBONZ_ACCOUNT_SID: '',
-      JAMBONZ_API_KEY: '',
-      JAMBONZ_REST_API_BASE_URL: '',
-      WEBHOOK_SECRET: '',
+      HTTP_PORT: 3001,
+      JAMBONZ_ACCOUNT_SID: '9351f46a-678c-43f5-b8a6-d4eb58d131af',
+      JAMBONZ_API_KEY: '1cf2f4f4-64c4-4249-9a3e-5bb4cb597c2a',
+      JAMBONZ_REST_API_BASE_URL: 'https://cpaas61.epacific.net/api/v1',
+      WEBHOOK_SECRET: 'wh_secret_cJqgtMDPzDhhnjmaJH6Mtk',
       CALLER_ID: ''
     }
   }]
